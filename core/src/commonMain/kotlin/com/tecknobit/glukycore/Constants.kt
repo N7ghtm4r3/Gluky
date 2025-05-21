f24a2115018c94b8f9d3d397fc8ccf8d995954b9
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-05-21 14:13:42 +0200

    Added Constants.kt

--- core/src/commonMain/kotlin/com/tecknobit/glukycore/Constants.kt
+++ core/src/commonMain/kotlin/com/tecknobit/glukycore/Constants.kt
@@ -2,6 +2,12 @@ package com.tecknobit.glukycore
 
 const val ANNOTATION_DATE_KEY = "annotation_date"
 
+const val CONTENT_KEY = "content"
+
+const val GLYCEMIA_KEY = "glycemia"
+
+const val RAW_CONTENT_KEY = "raw_content"
+
 const val INSULIN_UNITS_KEY = "insulin_units"
 
 const val POST_PRANDIAL_GLYCEMIA_KEY = "post_prandial_glycemia"
@@ -12,4 +18,4 @@ const val AFTERNOON_SNACK_KEY = "afternoon_snack"
 
 const val BASAL_INSULIN_KEY = "basal_insulin"
 
-const val RAW_CONTENT_KEY = "raw_content"
+const val DAILY_NOTES_KEY = "daily_notes"
