b71b4ab58d9b3f14792dd0e1ac42fef22d1ff7b0
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-05-23 15:14:09 +0200

    Added millis value to the GlycemicTrendPeriod

--- core/src/commonMain/kotlin/com/tecknobit/glukycore/enums/GlycemicTrendPeriod.kt
+++ core/src/commonMain/kotlin/com/tecknobit/glukycore/enums/GlycemicTrendPeriod.kt
@@ -3,14 +3,24 @@ package com.tecknobit.glukycore.enums
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class GlycemicTrendPeriod {
+enum class GlycemicTrendPeriod(
+    val millis: Long,
+) {
 
-    ONE_WEEK,
+    ONE_WEEK(
+        millis = 604_800_000L
+    ),
 
-    ONE_MONTH,
+    ONE_MONTH(
+        millis = 2_592_000_000L
+    ),
 
-    THREE_MONTHS,
+    THREE_MONTHS(
+        millis = 7_776_000_000L
+    ),
 
-    FOUR_MONTHS
+    FOUR_MONTHS(
+        millis = 10_368_000_000L
+    )
 
 }
