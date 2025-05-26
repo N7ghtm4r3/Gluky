e8c2011dd7be04830be4242a107c0c026032bb59
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-05-26 11:39:59 +0200

    Working on server configuration

---
+++ backend/src/main/java/com/tecknobit/gluky/configuration/CORSAdvice.java
@@ -0,0 +1,36 @@
+package com.tecknobit.gluky.configuration;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * The {@code CORSAdvice} class is useful to set the CORS policy
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ */
+@Configuration
+public class CORSAdvice {
+
+    /**
+     * Method used to set the CORS filter
+     */
+    @Bean
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
