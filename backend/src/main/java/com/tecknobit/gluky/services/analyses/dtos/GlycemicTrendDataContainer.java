25423006a74444c631140b35362af6d1d37e75c1
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-06-05 14:57:37 +0200

    Working on code documentation, documented the analyses dtos

--- backend/src/main/java/com/tecknobit/gluky/services/analyses/dtos/GlycemicTrendDataContainer.java
+++ backend/src/main/java/com/tecknobit/gluky/services/analyses/dtos/GlycemicTrendDataContainer.java
@@ -21,31 +21,76 @@ import static com.tecknobit.glukycore.ConstantsKt.*;
 import static com.tecknobit.glukycore.enums.GlycemicTrendLabelType.Companion;
 import static java.util.Calendar.*;
 
+/**
+ * The {@code GlycemicTrendDataContainer} class is used as DTO to share with the clients the data related to the
+ * glycemic trend over a selected period
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ */
 @DTO
 public class GlycemicTrendDataContainer {
 
+    /**
+     * {@code MAX_ALLOWED_SETS} the number of the maximum sets retrievable at once
+     */
     public static final int MAX_ALLOWED_SETS = 4;
 
+    /**
+     * {@code breakfast} the trend data related to the breakfast measurements
+     */
     private GlycemicTrendData breakfast;
 
+    /**
+     * {@code morningSnack} the trend data related to the morning snack measurements
+     */
     private GlycemicTrendData morningSnack;
 
+    /**
+     * {@code lunch} the trend data related to the lunch measurements
+     */
     private GlycemicTrendData lunch;
 
+    /**
+     * {@code afternoonSnack} the trend data related to the afternoon snack measurements
+     */
     private GlycemicTrendData afternoonSnack;
 
+    /**
+     * {@code dinner} the trend data related to the dinner measurements
+     */
     private GlycemicTrendData dinner;
 
+    /**
+     * {@code basalInsulin} the trend data related to the basal insulin measurements
+     */
     private GlycemicTrendData basalInsulin;
 
+    /**
+     * {@code period} the period used to retrieve the trend data
+     */
     private final GlycemicTrendPeriod period;
 
+    /**
+     * {@code dailyMeasurements} the measurements retrieved
+     */
     private final List<DailyMeasurements> dailyMeasurements;
 
+    /**
+     * {@code from} the start date from retrieve the measurements
+     */
     private final long from;
 
+    /**
+     * {@code to} The end date from retrieve the measurements
+     */
     private final long to;
 
+    /**
+     * Constructor to init the container
+     *
+     * @param period            The period used to retrieve the trend data
+     * @param dailyMeasurements The measurements retrieved
+     */
     public GlycemicTrendDataContainer(GlycemicTrendPeriod period, List<DailyMeasurements> dailyMeasurements) {
         this.period = period;
         this.dailyMeasurements = dailyMeasurements;
@@ -59,6 +104,9 @@ public class GlycemicTrendDataContainer {
         arrangeTrendData();
     }
 
+    /**
+     * Method used to arrange the trend data in the specific set
+     */
     private void arrangeTrendData() {
         GlycemicItemsOrganizer organizer = new GlycemicItemsOrganizer();
         for (MeasurementType type : MeasurementType.getEntries()) {
@@ -72,6 +120,14 @@ public class GlycemicTrendDataContainer {
         }
     }
 
+    /**
+     * Method used to load the specified trend data set
+     *
+     * @param type The type of the trend to load
+     * @param trendData The data used to load the specific set
+     *
+     * @param <T> the type of the measurement
+     */
     private <T extends GlycemicMeasurementItem> void loadSpecificTrend(MeasurementType type,
                                                                        HashMap<Integer, List<T>> trendData) {
         switch (type) {
@@ -84,84 +140,174 @@ public class GlycemicTrendDataContainer {
         }
     }
 
+    /**
+     * Method used to convert the raw trend data to the {@link GlycemicTrendData} object
+     *
+     * @param period The period used to retrieve the trend data 
+     * @param trendDataMap The map which contains the data to convert
+     *
+     * @return the converted data as {@link GlycemicTrendData}
+     * @param <T> the type of the measurement
+     */
     @Returner
     private <T extends GlycemicMeasurementItem> GlycemicTrendData convertToTrendData(
             GlycemicTrendPeriod period,
-            HashMap<Integer, List<T>> mealMap
+            HashMap<Integer, List<T>> trendDataMap
     ) {
-        if (mealMap == null || mealMap.isEmpty())
+        if (trendDataMap == null || trendDataMap.isEmpty())
             return null;
         return new GlycemicTrendData(
                 period,
-                mealMap
+                trendDataMap
         );
     }
 
+    /**
+     * Method used to get {@link #breakfast} instance
+     *
+     * @return {@link #breakfast} instance as {@link GlycemicTrendData}
+     */
     public GlycemicTrendData getBreakfast() {
         return breakfast;
     }
 
+    /**
+     * Method used to get {@link #morningSnack} instance
+     *
+     * @return {@link #morningSnack} instance as {@link GlycemicTrendData}
+     */
     @JsonGetter(MORNING_SNACK_KEY)
     public GlycemicTrendData getMorningSnack() {
         return morningSnack;
     }
 
+    /**
+     * Method used to get {@link #lunch} instance
+     *
+     * @return {@link #lunch} instance as {@link GlycemicTrendData}
+     */
     public GlycemicTrendData getLunch() {
         return lunch;
     }
 
+    /**
+     * Method used to get {@link #afternoonSnack} instance
+     *
+     * @return {@link #afternoonSnack} instance as {@link GlycemicTrendData}
+     */
     @JsonGetter(AFTERNOON_SNACK_KEY)
     public GlycemicTrendData getAfternoonSnack() {
         return afternoonSnack;
     }
 
+    /**
+     * Method used to get {@link #dinner} instance
+     *
+     * @return {@link #dinner} instance as {@link GlycemicTrendData}
+     */
     public GlycemicTrendData getDinner() {
         return dinner;
     }
 
+    /**
+     * Method used to get {@link #basalInsulin} instance
+     *
+     * @return {@link #basalInsulin} instance as {@link GlycemicTrendData}
+     */
     @JsonGetter(BASAL_INSULIN_KEY)
     public GlycemicTrendData getBasalInsulin() {
         return basalInsulin;
     }
 
+    /**
+     * Method used to get {@link #from} instance
+     *
+     * @return {@link #from} instance as {@code long}
+     */
     public long getFrom() {
         return from;
     }
 
+    /**
+     * Method used to get {@link #to} instance
+     *
+     * @return {@link #to} instance as {@code long}
+     */
     public long getTo() {
         return to;
     }
 
+    /**
+     * The {@code GlycemicTrendData} class is used to contains the trend data for each type of measurement
+     *
+     * @author N7ghtm4r3 - Tecknobit
+     */
     public static class GlycemicTrendData {
 
+        /**
+         * {@code type} the type of the label
+         */
         private final GlycemicTrendLabelType type;
 
+        /**
+         * {@code sets} container of each set
+         */
         private final List<GlycemiaPoint>[] sets;
 
+        /**
+         * {@code higherGlycemia} the higher glycemia value
+         */
         private final GlycemiaPoint higherGlycemia;
 
+        /**
+         * {@code lowerGlycemia} the lower glycemia value
+         */
         private final GlycemiaPoint lowerGlycemia;
 
+        /**
+         * {@code averageGlycemia} the average glycemia value
+         */
         private final double averageGlycemia;
 
+        /**
+         * Constructor to init the object
+         *
+         * @param period The period used to retrieve the trend data 
+         * @param mappedMeasurements The measurements data as map 
+         * @param <T> the type of the measurement
+         */
         public <T extends GlycemicMeasurementItem> GlycemicTrendData(GlycemicTrendPeriod period,
-                                                                     HashMap<Integer, List<T>> measurementsMapped) {
+                                                                     HashMap<Integer, List<T>> mappedMeasurements) {
             type = Companion.periodToRelatedLabel(period);
             sets = new List[MAX_ALLOWED_SETS];
-            loadSets(measurementsMapped);
+            loadSets(mappedMeasurements);
             higherGlycemia = findHigherGlycemia();
             lowerGlycemia = findLowerGlycemia();
             averageGlycemia = computeAverageGlycemia();
         }
 
-        private <T extends GlycemicMeasurementItem> void loadSets(HashMap<Integer, List<T>> measurementsMapped) {
+        /**
+         * Method used to load the {@link #sets}
+         *
+         * @param mappedMeasurements The measurements data as map
+         * @param <T>                the type of the measurement
+         */
+        private <T extends GlycemicMeasurementItem> void loadSets(HashMap<Integer, List<T>> mappedMeasurements) {
             int indexSet = 0;
-            for (List<T> measurements : measurementsMapped.values()) {
+            for (List<T> measurements : mappedMeasurements.values()) {
                 sets[indexSet] = convertToPoints(measurements);
                 indexSet++;
             }
         }
 
+        /**
+         * Method used to convert the measurements into {@link GlycemiaPoint} object
+         *
+         * @param measurements The measurements list to convert
+         *
+         * @return The converted measurements list converted as {@link List} of {@link GlycemiaPoint}
+         * @param <T> the type of the measurement
+         */
         @Assembler
         private <T extends GlycemicMeasurementItem> List<GlycemiaPoint> convertToPoints(List<T> measurements) {
             if (measurements == null || measurements.isEmpty())
@@ -172,6 +318,11 @@ public class GlycemicTrendDataContainer {
             return points;
         }
 
+        /**
+         * Method used to find the higher glycemia point
+         *
+         * @return the higher glycemia point as {@link GlycemiaPoint}
+         */
         private GlycemiaPoint findHigherGlycemia() {
             GlycemiaPoint[] higherPoints = new GlycemiaPoint[MAX_ALLOWED_SETS];
             for (int j = 0; j < MAX_ALLOWED_SETS; j++) {
@@ -193,6 +344,11 @@ public class GlycemicTrendDataContainer {
             return higherGlycemiaPoint;
         }
 
+        /**
+         * Method used to find the lower glycemia point
+         *
+         * @return the lower glycemia point as {@link GlycemiaPoint}
+         */
         private GlycemiaPoint findLowerGlycemia() {
             GlycemiaPoint[] lowerPoints = new GlycemiaPoint[MAX_ALLOWED_SETS];
             for (int j = 0; j < MAX_ALLOWED_SETS; j++) {
@@ -214,6 +370,11 @@ public class GlycemicTrendDataContainer {
             return lowerGlycemiaPoint;
         }
 
+        /**
+         * Method used to compute the average glycemia value in the {@link #period}
+         *
+         * @return the average glycemia value as {@code double}
+         */
         private double computeAverageGlycemia() {
             double totalGlycemias = 0;
             int totalRecords = 0;
@@ -229,50 +390,109 @@ public class GlycemicTrendDataContainer {
             return totalGlycemias / totalRecords;
         }
 
+        /**
+         * Method used to get {@link #type} instance
+         *
+         * @return {@link #type} instance as {@link GlycemicTrendLabelType}
+         */
         @JsonGetter(GLYCEMIC_LABEL_TYPE_KEY)
         public GlycemicTrendLabelType getType() {
             return type;
         }
 
+        /**
+         * Method used to get the first set
+         *
+         * @return the first set as {@link List} of {@link GlycemiaPoint}
+         */
         @JsonGetter(FIRST_SET_KEY)
         public List<GlycemiaPoint> getFirstSet() {
             return sets[0];
         }
 
+        /**
+         * Method used to get the second set
+         *
+         * @return the second set as {@link List} of {@link GlycemiaPoint}
+         */
         @JsonGetter(SECOND_SET_KEY)
         public List<GlycemiaPoint> getSecondSet() {
             return sets[1];
         }
 
+        /**
+         * Method used to get the second set
+         *
+         * @return the second set as {@link List} of {@link GlycemiaPoint}
+         */
         @JsonGetter(THIRD_SET_KEY)
         public List<GlycemiaPoint> getThirdSet() {
             return sets[2];
         }
 
+        /**
+         * Method used to get the fourth set
+         *
+         * @return the fourth set as {@link List} of {@link GlycemiaPoint}
+         */
         @JsonGetter(FOURTH_SET_KEY)
         public List<GlycemiaPoint> getFourthSet() {
             return sets[3];
         }
 
+        /**
+         * Method used to get {@link #higherGlycemia} instance
+         *
+         * @return {@link #higherGlycemia} instance as {@link GlycemiaPoint}
+         */
         @JsonGetter(HIGHER_GLYCEMIA_KEY)
         public GlycemiaPoint getHigherGlycemia() {
             return higherGlycemia;
         }
 
+        /**
+         * Method used to get {@link #lowerGlycemia} instance
+         *
+         * @return {@link #lowerGlycemia} instance as {@link GlycemiaPoint}
+         */
         @JsonGetter(LOWER_GLYCEMIA_KEY)
         public GlycemiaPoint getLowerGlycemia() {
             return lowerGlycemia;
         }
 
+        /**
+         * Method used to get {@link #averageGlycemia} instance
+         *
+         * @return {@link #averageGlycemia} instance as {@code double}
+         */
         @JsonGetter(AVERAGE_GLYCEMIA_KEY)
         public double getAverageGlycemia() {
             return averageGlycemia;
         }
 
+        /**
+         * The {@code GlycemiaPoint} record class is useful to represent a value on a chart
+         *
+         * @param date The date when the value has been annotated
+         * @param value The glycemic value
+         *
+         * @author N7ghtm4r3 - Tecknobit
+         */
         public record GlycemiaPoint(long date, double value) {
 
+            /**
+             * The {@code GlycemiaPointComparator} class is used to compare the {@link GlycemiaPoint} to correctly
+             * order them in the sets
+             *
+             * @author N7ghtm4r3 - Tecknobit
+             *
+             * @see java.util.Comparator
+             */
             public static final class GlycemiaPointComparator implements Comparator<GlycemiaPoint> {
 
+                /**
+                 * {@inheritDoc}
+                 */
                 @Override
                 public int compare(GlycemiaPoint o1, GlycemiaPoint o2) {
                     return (int) (o1.value - o2.value);
@@ -284,8 +504,24 @@ public class GlycemicTrendDataContainer {
 
     }
 
+    /**
+     * The {@code GlycemicItemsOrganizer} utility class is used to organize items in a list into a map,
+     * using the period as the key to group the items into specific lists
+     *
+     * @author N7ghtm4r3 - Tecknobit
+     */
+
     private static class GlycemicItemsOrganizer {
 
+        /**
+         * Method used to perform the organization routine
+         *
+         * @param period The period used to retrieve the trend data 
+         * @param items The items list to organize
+         *
+         * @return the map with the items organized as {@link HashMap} of {@link Integer} of {@link List} of {@link T}
+         * @param <T> the type of the measurement
+         */
         @Returner
         public <T extends GlycemicMeasurementItem> HashMap<Integer, List<T>> perform(
                 GlycemicTrendPeriod period,
@@ -298,6 +534,13 @@ public class GlycemicTrendDataContainer {
             };
         }
 
+        /**
+         * Method used to organize the items per a single week
+         *
+         * @param items The items list to organize 
+         * @return the map with the items organized as {@link HashMap} of {@link Integer} of {@link List} of {@link T}
+         * @param <T> the type of the measurement
+         */
         @Wrapper
         private <T extends GlycemicMeasurementItem> HashMap<Integer, List<T>> organizeSingleWeek(
                 List<T> items
@@ -305,6 +548,13 @@ public class GlycemicTrendDataContainer {
             return organize(items, t -> 0);
         }
 
+        /**
+         * Method used to organize the items per week, for example: first week, second week, etc...
+         *
+         * @param items The items list to organize 
+         * @return the map with the items organized as {@link HashMap} of {@link Integer} of {@link List} of {@link T}
+         * @param <T> the type of the measurement
+         */
         @Wrapper
         private <T extends GlycemicMeasurementItem> HashMap<Integer, List<T>> organizePerWeek(
                 List<T> items
@@ -319,6 +569,13 @@ public class GlycemicTrendDataContainer {
             return Math.min(weekOfMonth, MAX_ALLOWED_SETS);
         }
 
+        /**
+         * Method used to organize the items per week, for example: April, May, June, etc...
+         *
+         * @param items The items list to organize 
+         * @return the map with the items organized as {@link HashMap} of {@link Integer} of {@link List} of {@link T}
+         * @param <T> the type of the measurement
+         */
         @Wrapper
         private <T extends GlycemicMeasurementItem> HashMap<Integer, List<T>> organizePerMonth(
                 List<T> items
@@ -332,6 +589,15 @@ public class GlycemicTrendDataContainer {
             return calendar.get(MONTH);
         }
 
+        /**
+         * Core method used to organize the items
+         *
+         * @param items The items list to organize 
+         * @param getOrganizerKey The lambda used to retrieve the key to organize the items
+         *
+         * @return the map with the items organized as {@link HashMap} of {@link Integer} of {@link List} of {@link T}
+         * @param <T> the type of the measurement
+         */
         private <T extends GlycemicMeasurementItem> HashMap<Integer, List<T>> organize(
                 List<T> items,
                 Function<T, Integer> getOrganizerKey
--- backend/src/main/java/com/tecknobit/gluky/services/analyses/dtos/Report.java
+++ backend/src/main/java/com/tecknobit/gluky/services/analyses/dtos/Report.java
@@ -6,6 +6,14 @@ import com.tecknobit.equinoxcore.annotations.DTO;
 import static com.tecknobit.glukycore.ConstantsKt.REPORT_IDENTIFIER_KEY;
 import static com.tecknobit.glukycore.ConstantsKt.REPORT_URL;
 
+/**
+ * The {@code Report} record class is used as DTO to share with the clients the report created
+ *
+ * @param reportId  The identifier of the report
+ * @param name      The name of the report
+ * @param reportUrl The url where the report can be downloaded
+ * @author N7ghtm4r3 - Tecknobit
+ */
 @DTO
 public record Report(
         @JsonGetter(REPORT_IDENTIFIER_KEY) String reportId,
